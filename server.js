4dbecdee4333c7d204178b5f37f49d5962a1560f
Maithreyi Varma <maithreyi@gmailcom>
2023-02-26 11:05:17 +0530

    changing default route

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("frontend"))
-app.get("/html",function(req,res){
+app.get("/",function(req,res){
     res.sendFile(__dirname+"/frontend/index.html")
 })
-app.get("/",function(req,res){
-    res.send("Hello World");
-})
+
 app.listen(3000, function(){
     console.log("server running on http://localhost:3000")
 })
