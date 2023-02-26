d6d7910d930b2d5672f22f66fa17cbc771affc3c
Maithreyi Varma <maithreyi@gmailcom>
2023-02-26 11:02:41 +0530

    completed hello world application

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+
+app.use(express.static("frontend"))
+app.get("/html",function(req,res){
+    res.sendFile(__dirname+"/frontend/index.html")
+})
 app.get("/",function(req,res){
     res.send("Hello World");
-})
-app.get("/myname",function(req,res){
-    res.send("Maithreyi");
 })
 app.listen(3000, function(){
     console.log("server running on http://localhost:3000")
 })
+
